d2ac38354552774dfa8460c9f46addcbf43674a4
CheboiDerrick <derrick.cheboi@student.moringaschool.com>
2021-09-27 23:23:00 +0300

    added new blog post view and template

--- app/main/views.py
+++ app/main/views.py
@@ -15,3 +15,9 @@ def index():
     education = Blog.query.filter_by(category = 'Education').all()
     title='Blogs | Home'
     return render_template('index.html', education = education,technology = technology, blogs = blogs, health=health, title=title)
+
+@main.route('/blogs/<category_name>')
+def category(category_name):
+    category = Blog.query.filter_by(category=category_name).all() 
+    title=f'{category_name} Pitches'
+    return render_template('category.html', category = category, title=title, category_name=category_name)
